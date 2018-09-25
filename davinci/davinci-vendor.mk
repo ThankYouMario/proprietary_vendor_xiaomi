bf791284206b1ccbea7f9ab3a0408ef65f4cd84f
Bruno Martins <bgcngm@gmail.com>
2018-09-25 23:44:44 +0100

    davinci: Ship proper postprocessing soundfx libs
     * The lib responsible for audio postprocessing (libvolumelistener) from Xiaomi causes stuttering distorted audio. Replace with a working copy from enchilada.
    
    Change-Id: I46077aa463ce1939b123b8b4131e03a10dfe6d30

--- davinci/davinci-vendor.mk
+++ davinci/davinci-vendor.mk
@@ -95,7 +95,9 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/davinci/proprietary/lib64/vendor.qti.ims.callinfo@1.0.so:system/lib64/vendor.qti.ims.callinfo@1.0.so \
     vendor/xiaomi/davinci/proprietary/lib64/vendor.qti.ims.rcsconfig@1.0.so:system/lib64/vendor.qti.ims.rcsconfig@1.0.so \
     vendor/xiaomi/davinci/proprietary/lib64/vendor.qti.imsrtpservice@1.0.so:system/lib64/vendor.qti.imsrtpservice@1.0.so \
-    vendor/xiaomi/davinci/proprietary/lib64/vendor.qti.latency@2.0.so:system/lib64/vendor.qti.latency@2.0.so
+    vendor/xiaomi/davinci/proprietary/lib64/vendor.qti.latency@2.0.so:system/lib64/vendor.qti.latency@2.0.so \
+    vendor/xiaomi/davinci/proprietary/lib/soundfx/libvolumelistener.so:system/lib/soundfx/libvolumelistener.so \
+    vendor/xiaomi/davinci/proprietary/lib64/soundfx/libvolumelistener.so:system/lib64/soundfx/libvolumelistener.so
 
 PRODUCT_PACKAGES += \
     QtiTelephonyService \
