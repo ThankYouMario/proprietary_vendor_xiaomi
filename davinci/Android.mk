ba83b4d8c622f4717da30e4987a35b277615c155
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2020-01-19 22:38:34 +0100

    davinci: Build libqcomfm_jni as module
     * Needed by FM2 App
     * This reverts commit 4fc1938808e3957feb23211e87339a0c594d6628
    
    Change-Id: I9ce102ebdbdd75ce03c1d957e8de48f9f8506236

--- davinci/Android.mk
+++ davinci/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Paranoid Android
+# Copyright (C) 2019-2020 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
