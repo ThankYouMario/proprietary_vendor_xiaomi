6d60ef250d868f966499e2584b40db7887223dc8
Akash <akashniki@gmail.com>
2020-03-02 20:23:45 +0000

    cepheus: Add back hotwordenrollment blobs

--- cepheus/cepheus-vendor.mk
+++ cepheus/cepheus-vendor.mk
@@ -894,6 +894,8 @@ PRODUCT_PACKAGES += \
     pasrservice \
     QtiTelephonyService \
     uceShimService \
+    HotwordEnrollmentOKGoogleHEXAGON \
+    HotwordEnrollmentXGoogleHEXAGON \
     dpmserviceapp \
     ims \
     qcrilmsgtunnel
