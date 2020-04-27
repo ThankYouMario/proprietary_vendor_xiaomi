7992450015c9ef9e8f388262fce13f62efd735ae
Park Ju Hyung <qkrwngud825@gmail.com>
2020-04-27 11:05:12 +0900

    rosy: remove prebuilt libbtconfigstore
    This can be built from source and it screws up other devices
    that needs libbtconfigstore.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- rosy/rosy-vendor.mk
+++ rosy/rosy-vendor.mk
@@ -2370,7 +2370,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/rosy/proprietary/vendor/radio/qcril_database/qcril.db:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/qcril.db
 
 PRODUCT_PACKAGES += \
-    libbtconfigstore \
     libgpustats \
     libsdm-disp-vndapis \
     libthermalclient \
