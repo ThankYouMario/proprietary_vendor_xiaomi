9044c3b0cf150946e0087ca960ad72a4aa51833a
Vishalcj17 <vishalcj@aospa.co>
2020-07-27 21:31:48 +0530

    laurel_sprout: Initial import
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>

---
+++ laurel_sprout/proprietary/vendor/radio/qcril_database/upgrade/4_version_update_ecc_table.sql
@@ -0,0 +1,12 @@
+/*
+  Copyright (c) 2018 Qualcomm Technologies, Inc.
+  All Rights Reserved.
+  Confidential and Proprietary - Qualcomm Technologies, Inc.
+*/
+
+BEGIN TRANSACTION;
+
+INSERT OR REPLACE INTO qcril_properties_table (property, value) VALUES ('qcrildb_version', 4);
+
+
+COMMIT TRANSACTION;
